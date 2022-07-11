79dfeb8173ef14d31655305a8e99bf5eb358a629
edwin raj <you@example.com>
2022-07-11 10:32:21 +0530

    props example

---
+++ src/App.js
@@ -0,0 +1,36 @@
+// import React, {Component}  from "react";
+// import Home from "./Home";
+
+// class App extends Component{
+//   render(){
+//     const bike={
+//       name:'Nenja H2R',
+//       CC:950,
+//       Color:'Dark Grey'
+//     }
+//     console.log(bike);
+//     return(
+//       <div>
+//         < Home obj={bike}/>
+       
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+import React, {Component}  from "react";
+import Home from "./Home";
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        < Home name='Mr.eddy'/>
+       <Home para='hvbhkvhvfhdsfbx cnxc vsdvfxcbvxcz cxjnnchvbshdbcbchdc dsvsdhvsv cv'/>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,26 @@
+// import React from "react";
+
+// const Home=(props)=>{
+//     return(
+// <>
+// <h1>Home Page{props.bike}</h1>
+// </>
+//     )
+// }
+
+
+// export default Home;
+
+import React from "react";
+
+const Home=(props)=>{
+    return(
+<>
+<h1>Home Page  {props.name}</h1>
+<h1>{props.para}</h1>
+</>
+    )
+}
+
+
+export default Home;
